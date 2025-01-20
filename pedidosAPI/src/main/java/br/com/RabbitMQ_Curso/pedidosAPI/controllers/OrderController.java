de2f1d6ea38f1717f7f48a9f71a9e2c9b9ff44c2
Matheus Castilho <dev.mcastilho@gmail.com>
2025-01-20 11:17:14 -0300

    Create order controller

---
+++ pedidosAPI/src/main/java/br/com/RabbitMQ_Curso/pedidosAPI/controllers/OrderController.java
@@ -0,0 +1,21 @@
+package br.com.RabbitMQ_Curso.pedidosAPI.controllers;
+
+import br.com.RabbitMQ_Curso.pedidosAPI.models.OrderEntity;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/orders")
+public class OrderController {
+    private final Logger logger = LoggerFactory.getLogger(OrderController.class);
+
+    public ResponseEntity <OrderEntity> createOrder(@RequestBody OrderEntity order){
+        logger.info("Order received: {}", order.toString());
+        return ResponseEntity.status(HttpStatus.CREATED).body(order);
+    }
+}
